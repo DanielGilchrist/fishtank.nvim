6d96d911d5d852795aca0fdd4bf080871c4bedf1
Daniel Gilchrist <daniel@workforce.com>
2025-09-04 10:15:26 +0100

    Allow customising fish color

--- lua/fishtank/init.lua
+++ lua/fishtank/init.lua
@@ -1,13 +1,12 @@
-local options = require('fishtank.options')
-local setup = require('fishtank.setup')
-
 local M = {}
 
 M.setup = function(opts)
     -- set user options
+    local options = require('fishtank.options')
     options.setOptions(opts)
 
     -- setup
+    local setup = require('fishtank.setup')
     setup.setupUserCommand()
     setup.setupAutocommands()
     setup.setupScreensaver()
